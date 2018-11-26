2d2170ea0366721b423c8c5acd035386e7c722bb
Hayley <hayleyma16@gmail.com>
2018-11-25 21:49:57 -0600

    Overlay and Markers

--- ColoredMap/app/src/main/java/com/example/hayle/coloredmap/MapsActivity.java
+++ ColoredMap/app/src/main/java/com/example/hayle/coloredmap/MapsActivity.java
@@ -1,5 +1,6 @@
 package com.example.hayle.coloredmap;
 
+import android.graphics.Point;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -15,6 +16,7 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.GroundOverlay;
 import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
@@ -26,10 +28,11 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     private static final int TRANSPARENCY_MAX = 100;
     private GroundOverlay mGroundOverlay;
-    private GroundOverlay mGroundOverlayRotated;
+    //private GroundOverlay mGroundOverlayRotated;
+
 
     private static final LatLng CENTRAL_LOT = new LatLng(30.405803686603857, -91.17964625358582);
-    private static final LatLng EAST_LOT = new LatLng(30.405118946087722,-91.17716789245605);
+    private static final LatLng EAST_LOT = new LatLng(30.40510969280462,-91.17716789245605);
 
     //array to hold blue, green, red, pink, pics for the overlays
     private final List<BitmapDescriptor> mImages = new ArrayList<BitmapDescriptor>();
@@ -50,14 +53,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                     .tilt(50)
                     .build();
 
-    /**
-     * Toggles the visibility between 100% and 50% when a {@link GroundOverlay} is clicked.
-     */
-    @Override
-    public void onGroundOverlayClick(GroundOverlay groundOverlay) {
-        // Toggle transparency value between 0.0f and 0.5f. Initial default value is 0.0f.
-        mGroundOverlayRotated.setTransparency(0.5f - mGroundOverlayRotated.getTransparency());
-    }
+
+
 
     /**
      * Manipulates the map once available.
@@ -74,7 +71,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
 
         mMap = googleMap;
-
+        mMap.setOnGroundOverlayClickListener(this);
         // Add a marker at LSU and move the camera
         LatLng lsu = new LatLng(30.4133, -91.1800);
         //mMap.addMarker(new MarkerOptions().position(lsu).title("Marker at LSU"));
@@ -82,14 +79,44 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         //changeCamera(CameraUpdateFactory.newCameraPosition(lsu));
 
         mImages.clear();
-        mImages.add(BitmapDescriptorFactory.fromResource(R.drawable.bluesquare));
+        mImages.add(BitmapDescriptorFactory.fromResource(R.drawable.pft_central_lot));
+        mImages.add(BitmapDescriptorFactory.fromResource(R.drawable.pft_east_lot));
+
 
 
         //small overlay on central parking lot
         mGroundOverlay = googleMap.addGroundOverlay(new GroundOverlayOptions()
-        .image(mImages.get(0)).anchor(0,1)
-        .position(CENTRAL_LOT, 300F, 300F)//size and location
-        .bearing(30));
+                .image(mImages.get(0)).anchor((float).40,(float).45)
+                .position(CENTRAL_LOT, 410F, 300F)//size and location
+                .bearing(0) //rotation
+                .transparency((float) 0.5));
+
+        //small overlay on east parking lot
+        mGroundOverlay = googleMap.addGroundOverlay(new GroundOverlayOptions()
+                .image(mImages.get(1)).anchor((float).30,(float).55)
+                .position(EAST_LOT, 400F, 250F)//size and location
+                .bearing(28) //rotation
+                .transparency((float) 0.5)
+                .clickable(true));
+        mGroundOverlay.setTag("871/1264");
+        /*
+        MarkerOptions central = mMap.addMarker(new MarkerOptions()
+                                .position(CENTRAL_LOT).icon(null)
+                                .title("Central Lot").snippet("871/1275"));
+         */
+        MarkerOptions central = new MarkerOptions()
+                                .position(CENTRAL_LOT)
+                                .title("Central Lot").snippet("439/1264");
+        MarkerOptions east = new MarkerOptions()
+                .position(EAST_LOT)
+                .title("Central Lot").snippet("597/1275");
+        mMap.addMarker(central).setIcon(null);
+        mMap.addMarker(east).setIcon(null);
+
     }
 
+    @Override
+    public void onGroundOverlayClick(GroundOverlay groundOverlay) {
+        mGroundOverlay.setTag("871/1264");
+    }
 }
