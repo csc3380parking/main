4fcdac108abcb0ed74435686450dedfa309aca13
Hayley <hayleyma16@gmail.com>
2018-11-27 10:36:13 -0600

    Avail Spaces +/-

--- ColoredMap/app/src/main/java/com/example/hayle/coloredmap/MapsActivity.java
+++ ColoredMap/app/src/main/java/com/example/hayle/coloredmap/MapsActivity.java
@@ -1,9 +1,12 @@
 package com.example.hayle.coloredmap;
 
+import android.content.Context;
 import android.graphics.Point;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.style.UpdateAppearance;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -28,15 +31,17 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     private static final int TRANSPARENCY_MAX = 100;
     private GroundOverlay mGroundOverlay;
-    //private GroundOverlay mGroundOverlayRotated;
-
+    private GroundOverlay eGroundOverlay;
 
     private static final LatLng CENTRAL_LOT = new LatLng(30.405803686603857, -91.17964625358582);
     private static final LatLng EAST_LOT = new LatLng(30.40510969280462,-91.17716789245605);
 
+    Marker central, east;
     //array to hold blue, green, red, pink, pics for the overlays
     private final List<BitmapDescriptor> mImages = new ArrayList<BitmapDescriptor>();
 
+    public int centralAvail = 800;
+    public int eastAvail = 400;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,18 +57,9 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                     .bearing(300)
                     .tilt(50)
                     .build();
-
-
-
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    /** Manipulates the map once available. This callback is triggered when the map is ready to be used. This is where we can add markers or lines, add listeners or move the camera.
+     * If Google Play services is not installed on the device, the user will be prompted to install it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app. */
     private void changeCamera(CameraUpdate update) {
         changeCamera(update);
     }
@@ -82,41 +78,48 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mImages.add(BitmapDescriptorFactory.fromResource(R.drawable.pft_central_lot));
         mImages.add(BitmapDescriptorFactory.fromResource(R.drawable.pft_east_lot));
 
-
-
         //small overlay on central parking lot
         mGroundOverlay = googleMap.addGroundOverlay(new GroundOverlayOptions()
                 .image(mImages.get(0)).anchor((float).40,(float).45)
                 .position(CENTRAL_LOT, 410F, 300F)//size and location
                 .bearing(0) //rotation
-                .transparency((float) 0.5));
-
+                .transparency((float) 0.5)
+                .clickable(true));
+        mGroundOverlay.setTag("central");
         //small overlay on east parking lot
-        mGroundOverlay = googleMap.addGroundOverlay(new GroundOverlayOptions()
+        eGroundOverlay = googleMap.addGroundOverlay(new GroundOverlayOptions()
                 .image(mImages.get(1)).anchor((float).30,(float).55)
                 .position(EAST_LOT, 400F, 250F)//size and location
                 .bearing(28) //rotation
                 .transparency((float) 0.5)
                 .clickable(true));
-        mGroundOverlay.setTag("871/1264");
+        eGroundOverlay.setTag("east");
+        central = mMap.addMarker(new MarkerOptions()
+                                .position(CENTRAL_LOT).alpha(0)
+                                .title("Central Lot").snippet(centralAvail+"/1264"));
+        east = mMap.addMarker(new MarkerOptions()
+                .position(EAST_LOT).alpha(0)
+                .title("Central Lot").snippet(eastAvail+"/1275"));
         /*
-        MarkerOptions central = mMap.addMarker(new MarkerOptions()
-                                .position(CENTRAL_LOT).icon(null)
-                                .title("Central Lot").snippet("871/1275"));
-         */
         MarkerOptions central = new MarkerOptions()
                                 .position(CENTRAL_LOT).alpha(0)
                                 .title("Central Lot").snippet("439/1264");
-        MarkerOptions east = new MarkerOptions()
-                .position(EAST_LOT).alpha(0)
-                .title("Central Lot").snippet("597/1275");
         mMap.addMarker(central);
-        mMap.addMarker(east);
+        mMap.addMarker(east);*/
 
     }
 
     @Override
     public void onGroundOverlayClick(GroundOverlay groundOverlay) {
-        mGroundOverlay.setTag("871/1264");
+        //checks what lot was selected
+        if(groundOverlay.getTag()=="central"){
+            //will add or decrement based on buttons we add later
+            centralAvail--;
+            central.setSnippet(centralAvail+"/1264");
+        }
+        else if(groundOverlay.getTag()=="east"){
+            eastAvail--;
+            east.setSnippet(eastAvail+"/1275");
+        }
     }
 }
